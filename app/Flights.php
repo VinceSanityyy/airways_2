b73b8cd090b24bdca51c31ab612ad2ee2a94a21e
vince <vbustillo97@gmail.com>
2019-09-03 17:47:18 +0800

    6

--- app/Flights.php
+++ app/Flights.php
@@ -8,7 +8,7 @@ class Flights extends Model
 {
     public $timestamps = false;
     public $fillable = [
-        'flightFrom', 'flightTo', 'flightDepart',  'flight_id', 'alias','flight_schedule','flight_arrival'
+       'flight_id', 'alias','flight_schedule','flight_arrival'
         ,'terminal','flight_country_from','flight_country_to','plane_id','price','status'
     ];
 }
