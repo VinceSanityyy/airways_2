8be312bd98f4d6828e710eca5f9e60adea2d6f91
vince <vbustillo97@gmail.com>
2019-09-03 17:40:52 +0800

    5

--- app/Flights.php
+++ app/Flights.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Flights extends Model
 {
-    protected $timestamps = false;
-    protected $fillable = [
+    public $timestamps = false;
+    public $fillable = [
         'flightFrom', 'flightTo', 'flightDepart',  'flight_id', 'alias','flight_schedule','flight_arrival'
         ,'terminal','flight_country_from','flight_country_to','plane_id','price','status'
     ];
